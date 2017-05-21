ea36ef48ae879ccbb52413197fb8ef4946b43ae5
zxcpoiu <zxcpoiu@gmail.com>
2017-05-21 19:02:33 +0800

    lazy rtcp-mux negotiate

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -211,6 +211,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                 }
             }
         }
+        conf.rtcpMuxPolicy = PeerConnection.RtcpMuxPolicy.NEGOTIATE;
 
         // FIXME: peerIdentity of type DOMString (public api)
         // FIXME: certificates of type sequence<RTCCertificate> (public api)
